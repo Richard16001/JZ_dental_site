2f73e36c5bcd4e603a93fec9be85882f37ad31b0
Richard1601 <richard.chungjen@gmail.com>
2024-04-30 09:19:51 -0700

    rough draft finished (needs filling in and polishing. Like Teeth)

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,68 +1,92 @@
-import { Box, Typography, Container, Grid, createTheme } from '@mui/material'
+import { Box, Typography, Container, Grid, createTheme, Stack} from '@mui/material'
 import { bannerGirl } from '../assets'
-import { Stack } from '@mui/material'
+import TealButton from './TealButton'
+import YellowButton from './YellowButton'
 import React from 'react'
 
-const HeroTheme = createTheme({
-  Box: {
-  },
-  typography: {
-    button: {
-      textTransform: 'none',
-      fontFamily: 'outfit',
-      fontSize: 18
-    }
-  }
-})
+// const HeroTheme = createTheme({
+//   Box: {
+//   },
+//   typography: {
+//     button: {
+//       textTransform: 'none',
+//       fontFamily: 'outfit',
+//       fontSize: 18
+//     }
+//   }
+// })
 
 
 function Hero() {
   return (
     <Box
       sx={{
-        position: 'relative',
         display: 'flex',
         pt: 8
       }}
     >
-      {/* Banner image */}
-      <Box 
-        component="img" 
-        src={bannerGirl} 
-        alt={`banner girl`} 
-        sx={{ 
-          maxWidth: '100%'
-        }}
-      />
-      {/* Title text and button */}
-      <Box
-         sx={{
-          display: 'flex',
-          position: 'absolute',
-          top: '60%',
-          left: '10%',
-          transform: 'translateY(-50%)',
-          bgcolor: '#FFFFFF',
-          color: '#4E5283',
-          maxWidth: '500px'
-         }}
-      >
+      <Container maxWidth='lg'>
+        <Grid container alignItems='center' justifyContent='center'>   
+          <Grid item sm={12} md={6}>
+            {/* Title text and button */}
+            <Box
+              sx={{
+                display: 'flex',
+                bgcolor: '#FFFFFF',
+                color: '#4E5283',
+                maxWidth: '500px',
+                pt: 6,
+                pl: 3,
+                pr: 3,
+                pb: 4
+              }}
+            >
 
-        <Stack spacing={2}>
-          <Box>
-            <Typography variant="h2">
-              Build a Healthy Smile Today
-            </Typography>
-          </Box>
-          <Box>
-            <Typography variant="h5">
-              Zarate Studios brings you dental care that’s worth smiling about. Come visit our offices today!
-            </Typography>
-          </Box>
-        </Stack>
+              <Stack spacing={2} justifyContent= 'flex-start'>
+                <Box>
+                  <Typography variant="h2">
+                    Build a Healthy Smile Today
+                  </Typography>
+                </Box>
+                <Box>
+                  <Typography variant="h5">
+                    Zarate Studios brings you dental care that’s worth smiling about. Come visit our offices today!
+                  </Typography>
+                </Box>
+                
+                <Grid container spacing={0} justifyContent='flex-start' display='flex'>
+                  <Grid item xs={6}>
+                    <YellowButton>Request Appointment </YellowButton>
+                  </Grid>
+                  <Grid item xs={6} sx={{pl: 2}}>
+                    <TealButton>Contact Us</TealButton>
+                  </Grid>
+                </Grid>
+                
 
-      </Box>
-      
+
+              </Stack>
+
+            </Box>
+          </Grid>   
+
+
+          <Grid item sm={12} md={6}>
+            
+            {/* Banner image */}
+            <Box 
+              display={{}}
+              component="img" 
+              src={bannerGirl} 
+              alt={`banner girl`} 
+              sx={{ 
+                maxWidth: '100%'
+              }}
+            />
+              
+          </Grid>  
+        </Grid>
+      </Container>
     </Box>
   )
 }
--- src/components/ServiceCards.jsx
+++ src/components/ServiceCards.jsx
@@ -1,7 +1,7 @@
-import { Box, Card, CardContent, Typography } from '@mui/material';
+import { Box, Card, CardContent, Typography } from '@mui/material'
 import { createTheme, ThemeProvider } from '@mui/material/styles'
 import { Grid } from '@mui/material'
-import TealButton from './TealButton';
+import TealButton from './TealButton'
 
 
 import { blueCardS, purpleCardS, redCardS, whiteCardS } from '../assets';
@@ -26,16 +26,16 @@ function ServiceCards() {
   return (
     <Box
       sx={{
+        // display: 'flex',
         position: 'relative',
         padding: 10,
       }}
     >
       <Grid container spacing={5}>
 
-        <Grid item sx={12} md={6} lg={3}>
-          <Card sx={{pb: 3, pl: 2, pr: 2, pt: 6}}>
+        <Grid item xs={12} md={6} lg={3}>
+          <Card sx={{pl: 2, pr: 2, pt: 6, height: '95%', bgcolor: '#D6E7EB'}}>
             <CardContent>
-              {/* card image */}
               <Box 
                 component="img" 
                 src={blueCardS} 
@@ -49,10 +49,10 @@ function ServiceCards() {
                   pb: 5
                 }}
               />
-              <Typography variant ="h4">
+              <Typography variant ="h4" >
                 Dental Procedures
               </Typography>
-              <Typography variant="body1">
+              <Typography variant="body1" sx={{border: 1, height: '50px'}}>
                 Check out our Cleanings, Exams, and X-rays.
               </Typography>
               
@@ -64,89 +64,95 @@ function ServiceCards() {
           </Card>
         </Grid>
 
-        <Grid item sx={12} md={6} lg={3}>
-          <Card>
+        <Grid item xs={12} md={6} lg={3}>
+          <Card sx={{pl: 2, pr: 2, pt: 6, height: '95%', bgcolor: '#D6E7EB'}}>
             <CardContent>
-              {/* card image */}
               <Box 
                 component="img" 
                 src={purpleCardS} 
                 alt={`purple card`} 
                 sx={{ 
-                  display: "flex",
-                  justifyContent: "center",
+                  display: 'flex',
+                  justifyContent: 'center',
                   maxHeight: '100%',
                   maxWidth: '100%',
                   height: 'auto',
-                  padding: 3
+                  pb: 5
                 }}
               />
-
               <Typography variant ="h4">
                 Cosmetic Procedures
               </Typography>
-              <Typography variant="body1">
+              <Typography variant="body1" sx={{border: 1, height: '50px'}}>
                 Check out our Teeth Whitening Services.
               </Typography>
+              
+              <Box sx={{justifyContent: 'center', pt: 2}}>
+                <TealButton>Learn More</TealButton>
+              </Box>
+
             </CardContent>
           </Card>
         </Grid>
-
-        <Grid item sx={12} md={6} lg={3}>
-          <Card>
+        
+        <Grid item xs={12} md={6} lg={3}>
+          <Card sx={{pl: 2, pr: 2, pt: 6, height: '95%', bgcolor: '#D6E7EB'}}>
             <CardContent>
-              
               <Box 
                 component="img" 
                 src={redCardS} 
                 alt={`red card`} 
                 sx={{ 
-                  display: "flex",
-                  justifyContent: "center",
+                  display: 'flex',
+                  justifyContent: 'center',
                   maxHeight: '100%',
                   maxWidth: '100%',
                   height: 'auto',
-                  padding: 3
+                  pb: 5
                 }}
               />
-
-
               <Typography variant ="h4">
                 Surgical Procedures
               </Typography>
-              <Typography variant="body1">
+              <Typography variant="body1" sx={{border: 1, height: '50px'}}>
                 Check out our Extractions, Fillings, and Root Canals.
               </Typography>
+              
+              <Box sx={{justifyContent: 'center', pt: 2}}>
+                <TealButton>Learn More</TealButton>
+              </Box>
+
             </CardContent>
           </Card>
         </Grid>
 
-        <Grid item sx={12} md={6} lg={3}>
-          <Card>
+        <Grid item xs={12} md={6} lg={3}>
+          <Card sx={{pl: 2, pr: 2, pt: 6, height: '95%', bgcolor: '#D6E7EB'}}>
             <CardContent>
-
-
               <Box 
                 component="img" 
                 src={whiteCardS} 
                 alt={`white card`} 
                 sx={{ 
-                  display: "flex",
-                  justifyContent: "center",
+                  display: 'flex',
+                  justifyContent: 'center',
                   maxHeight: '100%',
                   maxWidth: '100%',
                   height: 'auto',
-                  padding: 3
+                  pb: 5
                 }}
               />
-
-
               <Typography variant ="h4">
-                Orthodontic Procedures 
+                Orthodontic Procedures
               </Typography>
-              <Typography variant="body1">
-                Check out our Invisalign services. 
+              <Typography variant="body1" sx={{border: 1, height: '70px'}}>
+                Check out our Invisalign services.                                     
               </Typography>
+              
+              <Box sx={{justifyContent: 'center', pt: 2}}>
+                <TealButton>Learn More</TealButton>
+              </Box>
+
             </CardContent>
           </Card>
         </Grid>
